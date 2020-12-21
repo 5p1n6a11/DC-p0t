4acdd0bc36b0c6beb9f5ed04f68aaecbf7a532a1
u2i <pv3f3wii@s.okayama-u.ac.jp>
2020-12-21 22:23:14 +0900

    fix

--- log/get_log.sh
+++ log/get_log.sh
@@ -3,7 +3,7 @@
 sudo rm -rf log/docker_log/*
 sudo docker diff victim | sed 's/^C //g' | sed 's/^A //g' | while read line
 do
-    TOTAL=$(docker exec victim ls -l $line | head -n 1 | cut -c 1-5)
+    TOTAL=$(sudo docker exec victim ls -l $line | head -n 1 | cut -c 1-5)
     if test "$TOTAL" != "total" ; then
         mkdir -p log/docker_log/$(dirname $line)
         sudo docker cp victim:$line log/docker_log/$line
