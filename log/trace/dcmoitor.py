ac047ece41da44d7d49519e04ca7d73241960b11
u2i <pv3f3wii@s.okayama-u.ac.jp>
2020-12-25 11:16:34 +0900

    BPF Maps

--- log/trace/dcmoitor.py
+++ log/trace/dcmoitor.py
@@ -5,12 +5,33 @@ bpf_text = """
 #include <linux/sched.h>
 #include <linux/fs.h>
 
+BPF_HASH(hash);
+
 int do_trace_execve(struct pt_regs *ctx,
     const char __user *filename,
     const char __user *const __user *__argv,
     const char __user *const __user *__envp)
 {
-    bpf_trace_printk("Hello, World!\\n");
+    u64 key, value;
+    key = 1, value = 1234;
+
+    hash.update(&key, &value);
+
+    return 0;
+}
+
+int do_trace_ret_execve(struct pt_regs *ctx)
+{
+    u64 *p;
+    u64 key = 1;
+
+    p = hash.lookup(&key);
+    if (p == NULL) {
+        bpf_trace_printk("Not found\\n");
+        return 0;
+    }
+
+    bpf_trace_printk("%d\\n", *p);
     return 0;
 }
 """
@@ -19,6 +40,7 @@ b = BPF(text=bpf_text)
 
 execve_fnname = b.get_syscall_fnname("execve")
 b.attach_kprobe(event=execve_fnname, fn_name="do_trace_execve")
+b.attach_kretprobe(event=execve_fnname, fn_name="do_trace_ret_execve")
 
 while 1:
     try:
