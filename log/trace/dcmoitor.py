10e2e30699082966240c929fcc5348cc4f9c2818
u2i <pv3f3wii@s.okayama-u.ac.jp>
2020-12-25 11:42:06 +0900

    test code (unstable)

--- log/trace/dcmoitor.py
+++ log/trace/dcmoitor.py
@@ -1,4 +1,4 @@
-from bcc import BPF, libbcc
+from bcc import BPF, libbcc, table
 import ctypes
 
 bpf_text = """
@@ -27,6 +27,21 @@ int do_trace_execve(struct pt_regs *ctx,
 }
 """
 
+path = "/sys/fs/bpf/counter"
+
+class PinnedArray(table.Array):
+    def __init__(self, path, keytype, leaftype, max_entries):
+        map_fd = libbcc.lib.bpf_obj_get(ctypes.c_char_p(path.encode('utf-8')))
+
+        if map_fd < 0:
+            raise ValueError("Failed to open eBPF map")
+
+        self.map_fd = map_fd
+        self.key = keytype
+        self.Leaf = leaftype
+        self.max_entries = max_entries
+
+
 b = BPF(text=bpf_text)
 pin_path = "/sys/fs/bpf/counter"
 execve_fnname = b.get_syscall_fnname("execve")
@@ -37,9 +52,17 @@ ret = libbcc.lib.bpf_obj_pin(h.map_fd, ctypes.c_char_p(pin_path.encode('utf-8'))
 if ret != 0:
     raise Exception("Failed to pin map")
 
+
 while 1:
     try:
         b.trace_print()
     except KeyboardInterrupt:
+        counts = PinnedArray(
+            path = path,
+            keytype = ctypes.c_uint64,
+            leaftype = ctypes.c_uint64,
+            max_entries = 10240,
+        )
+        print(counts.values()[0].value)
         exit()
 
