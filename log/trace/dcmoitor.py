3174c6e7195bb279950b07753f482bcd7328ed3a
u2i <pv3f3wii@s.okayama-u.ac.jp>
2020-12-25 14:02:17 +0900

    trace execve system call in container

--- log/trace/dcmoitor.py
+++ log/trace/dcmoitor.py
@@ -4,39 +4,43 @@ bpf_text = """
 #include <uapi/linux/ptrace.h>
 #include <linux/sched.h>
 #include <linux/fs.h>
+#include <linux/nsproxy.h>
+#include <linux/ns_common.h>
+#include <linux/utsname.h>
+#include <linux/pid_namespace.h>
+#include <linux/proc_ns.h>
 
-BPF_PERF_OUTPUT(events);
-
-struct data_t {
-    char comm[TASK_COMM_LEN];
-    u32 pid;
-};
-
-int do_trace(struct pt_regs *ctx,
-    const char __user *filename,
-    const char __user *const __user *__argv,
-    const char __user *const __user *__envp)
+int trace_ret_execve(struct pt_regs *ctx)
 {
-    struct data_t data = {};
+    u32 pid;
+    struct task_struct *task;
+    char comm[TASK_COMM_LEN];
+    char nodename[9];
 
-    data.pid = bpf_get_current_pid_tgid();
-    bpf_get_current_comm(&data.comm, sizeof(data.comm));
-    events.perf_submit(ctx, &data, sizeof(data));
+    pid = bpf_get_current_pid_tgid() >> 32;
+    bpf_get_current_comm(&comm, sizeof(comm));
+
+    task = (struct task_struct *) bpf_get_current_task();
+    struct pid_namespace *pns = (struct pid_namespace *) task->nsproxy->pid_ns_for_children;
+
+    if (pns->ns.inum == PROC_PID_INIT_INO) {
+        return 0;
+    }
+
+    struct uts_namespace *uns = (struct uts_namespace *) task->nsproxy->uts_ns;
+
+    bpf_trace_printk("name: %s\\n", uns->name.nodename);
+    bpf_trace_printk("comm: %s, ns: %lld\\n", comm, pns->ns.inum);
     return 0;
 }
 """
 
-bpf = BPF(text=bpf_text)
-execve_fnname = bpf.get_syscall_fnname("execve")
-bpf.attach_kretprobe(event=execve_fnname, fn_name="do_trace")
+b = BPF(text=bpf_text)
+execve_fnname = b.get_syscall_fnname("execve")
+b.attach_kretprobe(event=execve_fnname, fn_name="trace_ret_execve")
 
-def print_event(cpu, data, size):
-    event = bpf["events"].event(data)
-    print("pid: %d, comm: %s" % (event.pid, event.comm))
-
-bpf["events"].open_perf_buffer(print_event)
 while 1:
     try:
-        bpf.perf_buffer_poll()
+        b.trace_print()
     except KeyboardInterrupt:
         exit()
