ef23050493f9a8e1ead8e33a0f9fe1f963485de4
u2i <pv3f3wii@s.okayama-u.ac.jp>
2021-01-24 13:53:47 +0900

    trace execve system call

--- log/trace/tracer.py
+++ log/trace/tracer.py
@@ -1,16 +1,9 @@
-#!/usr/bin/env python3
+#/usr/bin/env python3
 
-from __future__ import print_function
 from bcc import BPF
-from bcc.containers import filter_by_containers
-from bcc.utils import ArgString, printb
-import bcc.utils as utils
-import argparse
-import re
+from bcc.utils import printb
 import time
-import pwd
 from collections import defaultdict
-from time import strftime
 
 BPF_PROGRAM = "event_monitor_ebpf.c"
 
@@ -27,9 +20,36 @@ execve_fnname = b.get_syscall_fnname("execve")
 b.attach_kprobe(event=execve_fnname, fn_name="syscall__execve")
 b.attach_kretprobe(event=execve_fnname, fn_name="do_ret_sys_execve")
 
+print("%-16s %-6s %-6s %3s" % ("PCOMM", "PID", "PPID", "RET"))
+
+class EventType(object):
+    EVENT_ARG = 0
+    EVENT_RET = 1
+
+start_ts = time.time()
+argv = defaultdict(list)
+
+# process event
+def print_event(cpu, data, size):
+    event = b["events"].event(data)
+    skip = False
+
+    if event.type == EventType.EVENT_ARG:
+        pass
+    elif event.type == EventType.EVENT_RET:
+        if event.retval != 0:
+            skip = True
+
+        if not skip:
+            ppid = event.ppid if event.ppid > 0 else get_ppid(event.pid)
+            ppid = b"%d" % ppid if ppid > 0 else b"?"
+            printb(b"%-16s %-6d %-6s %3d" % (event.comm, event.pid,
+                    ppid, event.retval))
+
+# loop with callback to print_event
 b["events"].open_perf_buffer(print_event)
 while 1:
     try:
         b.perf_buffer_poll()
-    except KeyboradInterrupt:
+    except KeyboardInterrupt:
         exit()
