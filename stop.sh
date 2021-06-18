1fa49db17988a08af5c5cc2fbc835f912690ef72
u2i <62577784+5p1n6a11@users.noreply.github.com>
2021-06-18 11:27:10 +0900

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 ps aux | grep tcpdump | grep -v grep | awk '{ print "kill -INT", $2 }' | sudo sh
-ps aux | grep tracer.py | grep -v tracer.py | awk '{ print "kill -INT", $2 }' | sudo sh
+ps aux | grep tracer.py | grep -v grep | awk '{ print "kill -INT", $2 }' | sudo sh
 ./monitor/get_log.sh
 sudo docker stop victim
 sudo docker rm victim
